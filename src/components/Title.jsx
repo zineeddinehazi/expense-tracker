5b5751ada7e706892d4218361ff69f0046017fd0
zineeddinehazi <z.hazi.igee@univ-boumerdes.dz>
2025-05-16 00:14:39 +0100

    remove a useless class name

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 export default function Title() {
   return (
     <div className="mt-12 mb-16 flex justify-center text-5xl font-bold">
-      <h1 className="title">Expense Tracker</h1>
+      <h1>Expense Tracker</h1>
     </div>
   );
 }
